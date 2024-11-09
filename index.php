32232f820debbedcae7df3e7b2c10bc0cae8ece5
AlexCheow <alexcheow12@gmail.com>
2024-11-09 16:32:42 +0800

    Add Admin Dashboard

--- index.php
+++ index.php
@@ -17,14 +17,14 @@
 		<div class="container">
 			<div class="row justify-content-center">
 				<div class="col-md-6 text-center mb-5">
-					<h2 class="heading-section">Admin</h2>
+					<h1 class="heading-section">Welcome Admin</h1>
 				</div>
 			</div>
 			<div class="row justify-content-center">
 				<div class="col-md-6 col-lg-4">
 					<div class="login-wrap p-0">
-		      	<h3 class="mb-4 text-center">Have an account?</h3>
-		      	<form action="#" class="signin-form">
+		      	
+		      	<form action="dashboard.php" class="signin-form">
 		      		<div class="form-group">
 		      			<input type="text" class="form-control" placeholder="Username" required>
 		      		</div>
